6d5f9faccb631be7737817cb317eb5f0c0553d85
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-07 20:47:06 +0100

    Add Quote Form

---
+++ src/components/forms/QuoteForm.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { calculateQuote } from "../../utils/QuoteCalulation";
+import formStyles from "../../../styles/Form.module.css";
+
+function QuoteForm({ handleQuote }) {
+  const [age, setAge] = useState("");
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [isSmoker, setIsSmoker] = useState(false);
+
+  const handleValidateInput = (e) => {
+    const { name, value } = e.target;
+    const digitRegex = /\d/g;
+
+    const isValueValid = digitRegex.test(value);
+    if (isValueValid) {
+      if (name === "height") setHeight(parseInt(value));
+      if (name === "weight") setWeight(parseInt(value));
+    }
+  };
+
+  const handleCancel = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = { age, height, weight, isSmoker };
+    console.log(formData, "data");
+    const quote = calculateQuote(formData);
+    handleQuote(quote);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={formStyles.form}>
+      <div>
+        <label>Select Your Age</label>
+        <select name="age" value={age ? age : ""} onChange={(e) => setAge(parseInt(e.target.value))} required>
+          {new Array(150).fill().map((array, index) => (
+            <option key={index}>{index}</option>
+          ))}
+        </select>
+      </div>
+
+      <div>
+        <label>Height (cm)</label>
+        <input
+          id="height"
+          name="height"
+          value={height ? height : ""}
+          placeholder="Enter your Height (cm)..."
+          onChange={handleValidateInput}
+          required
+        />
+      </div>
+
+      <div>
+        <label>Weight (kg)</label>
+        <input
+          id="weight"
+          name="weight"
+          value={weight ? weight : ""}
+          placeholder="Enter your weight (kg)"
+          onChange={handleValidateInput}
+          required
+        />
+      </div>
+
+      <div>
+        <label>Smoker</label>
+        <input
+          type="checkbox"
+          id="smoker"
+          name="smoker"
+          defaultChecked={isSmoker ? isSmoker : false}
+          onChange={() => setIsSmoker(!isSmoker)}
+        />
+      </div>
+
+      <button onClick={handleCancel}>Cancel</button>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default QuoteForm;
